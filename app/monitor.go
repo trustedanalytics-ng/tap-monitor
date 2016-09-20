e1f5701dbc5aa7490ae814289e381ea25c7d2124
Jakub Wierzbowski <jakub.a.wierzbowski@intel.com>
2016-09-20 17:07:03 +0200

    DPNG-10722 Try to reconnect to QUEUE on error case (#7)
    * DPNG-10722 Try to reconnect to QUEUE on error case
    
    * DPNG-10722 Review refactor
    
    * DPNG-10722 Fix misspelling

--- app/monitor.go
+++ app/monitor.go
@@ -27,12 +27,16 @@ var deleteInstanceQueue = []string{}
 var pendingImageQueue = []string{}
 var readyImageQueue = []string{}
 
+type QueueManager struct {
+	*amqp.Channel
+	*amqp.Connection
+}
+
 func StartMonitor(waitGroup *sync.WaitGroup) {
 	waitGroup.Add(1)
-	channel, conn := queue.GetConnectionChannel()
-	queue.CreateExchangeWithQueueByRoutingKeys(channel, containerBrokerModels.CONTAINER_BROKER_QUEUE_NAME,
-		[]string{containerBrokerModels.CONTAINER_BROKER_CREATE_ROUTING_KEY, containerBrokerModels.CONTAINER_BROKER_DELETE_ROUTING_KEY})
-	queue.CreateExchangeWithQueueByRoutingKeys(channel, imageFactoryModels.IMAGE_FACTORY_QUEUE_NAME, []string{imageFactoryModels.IMAGE_FACTORY_IMAGE_ROUTING_KEY})
+
+	channel, conn := setupQueueConnection()
+	queueManager := &QueueManager{channel, conn}
 
 	isRunning := true
 	go func() {
@@ -41,22 +45,30 @@ func StartMonitor(waitGroup *sync.WaitGroup) {
 	}()
 
 	for isRunning {
-		if err := CheckCatalogRequestedInstances(channel); err != nil {
+		if err := queueManager.CheckCatalogRequestedInstances(); err != nil {
 			logger.Error("Proccessing Catalog instances error:", err)
 		}
-		if err := CheckCatalogRequestedImages(channel); err != nil {
+		if err := queueManager.CheckCatalogRequestedImages(); err != nil {
 			logger.Error("Proccessing Catalog images error:", err)
 		}
 		time.Sleep(CHECK_INTERVAL_SECONDS * time.Second)
 	}
 
-	defer conn.Close()
-	defer channel.Close()
+	defer queueManager.Connection.Close()
+	defer queueManager.Channel.Close()
 	logger.Info("Monitoring stopped")
 	waitGroup.Done()
 }
 
-func CheckCatalogRequestedImages(channel *amqp.Channel) error {
+func setupQueueConnection() (*amqp.Channel, *amqp.Connection) {
+	channel, conn := queue.GetConnectionChannel()
+	queue.CreateExchangeWithQueueByRoutingKeys(channel, containerBrokerModels.CONTAINER_BROKER_QUEUE_NAME,
+		[]string{containerBrokerModels.CONTAINER_BROKER_CREATE_ROUTING_KEY, containerBrokerModels.CONTAINER_BROKER_DELETE_ROUTING_KEY})
+	queue.CreateExchangeWithQueueByRoutingKeys(channel, imageFactoryModels.IMAGE_FACTORY_QUEUE_NAME, []string{imageFactoryModels.IMAGE_FACTORY_IMAGE_ROUTING_KEY})
+	return channel, conn
+}
+
+func (q *QueueManager) CheckCatalogRequestedImages() error {
 	images, _, err := config.CatalogApi.ListImages()
 	if err != nil {
 		logger.Error("Failed to ListImages:", err)
@@ -69,22 +81,13 @@ func CheckCatalogRequestedImages(channel *amqp.Channel) error {
 			if isInstanceIdInArray(pendingImageQueue, image.Id) {
 				continue
 			}
-
-			queueMessage, err := prepareBuildImageRequest(image)
-			if err != nil {
-				logger.Error("Failed to prepare BuildImagePostRequest for image: ", image.Id, err)
-				break
-			}
-			if err := queue.SendMessageToQueue(channel, queueMessage, imageFactoryModels.IMAGE_FACTORY_QUEUE_NAME,
-				imageFactoryModels.IMAGE_FACTORY_IMAGE_ROUTING_KEY); err != nil {
-				logger.Error("Can't send message on queue! ImageId:", image.Id, err)
-				break
-			}
 			pendingImageQueue = append(pendingImageQueue, image.Id)
+			q.sendMessageOnQueue(image, imageFactoryModels.IMAGE_FACTORY_QUEUE_NAME, containerBrokerModels.CONTAINER_BROKER_CREATE_ROUTING_KEY)
 		case catalogModels.ImageStateReady:
 			if isInstanceIdInArray(readyImageQueue, image.Id) {
 				continue
 			}
+			readyImageQueue = append(readyImageQueue, image.Id)
 
 			if catalogModels.IsApplicationInstance(image.Id) {
 				applicationId := catalogModels.GetApplicationId(image.Id)
@@ -116,7 +119,6 @@ func CheckCatalogRequestedImages(channel *amqp.Channel) error {
 					}
 				}
 			}
-			readyImageQueue = append(readyImageQueue, image.Id)
 		}
 	}
 	return nil
@@ -126,9 +128,7 @@ func getImageAddress(id string) string {
 	return docker_hub_address + "/" + id
 }
 
-//todo what we should do in error case: log error and continue or break/fail
-//todo should we change instance state to FAILURE on error case?
-func CheckCatalogRequestedInstances(channel *amqp.Channel) error {
+func (q *QueueManager) CheckCatalogRequestedInstances() error {
 	instances, _, err := config.CatalogApi.ListInstances()
 	if err != nil {
 		return err
@@ -139,44 +139,65 @@ func CheckCatalogRequestedInstances(channel *amqp.Channel) error {
 			if isInstanceIdInArray(createInstanceQueue, instance.Id) {
 				continue
 			}
-			go sendMessageOnQueue(instance, channel, containerBrokerModels.CONTAINER_BROKER_CREATE_ROUTING_KEY)
+			q.sendMessageOnQueue(instance, containerBrokerModels.CONTAINER_BROKER_QUEUE_NAME, containerBrokerModels.CONTAINER_BROKER_CREATE_ROUTING_KEY)
 			createInstanceQueue = append(createInstanceQueue, instance.Id)
 		} else if instance.State == catalogModels.InstanceStateDestroyReq {
 			if isInstanceIdInArray(deleteInstanceQueue, instance.Id) {
 				continue
 			}
 			deleteInstanceQueue = append(deleteInstanceQueue, instance.Id)
-			go sendMessageOnQueue(instance, channel, containerBrokerModels.CONTAINER_BROKER_DELETE_ROUTING_KEY)
+			q.sendMessageOnQueue(instance, containerBrokerModels.CONTAINER_BROKER_QUEUE_NAME, containerBrokerModels.CONTAINER_BROKER_DELETE_ROUTING_KEY)
 		}
 	}
 	return nil
 }
 
-func sendMessageOnQueue(instance catalogModels.Instance, channel *amqp.Channel, routingKey string) {
+func (q *QueueManager) sendMessageOnQueue(entity interface{}, queueName, routingKey string) {
 	var queueMessage []byte
 	var err error
 
 	switch routingKey {
+	case imageFactoryModels.IMAGE_FACTORY_IMAGE_ROUTING_KEY:
+		image, _ := entity.(catalogModels.Image)
+		queueMessage, err = prepareBuildImageRequest(image)
+		if err != nil {
+			logger.Error("Failed to prepare BuildImagePostRequest for image: ", image.Id, err)
+			break
+		}
+
 	case containerBrokerModels.CONTAINER_BROKER_CREATE_ROUTING_KEY:
+		instance, _ := entity.(catalogModels.Instance)
 		queueMessage, err = prepareCreateInstanceRequest(instance)
 		if err != nil {
 			logger.Error("Failed to prepare CreateInstanceRequest for instance: ", instance.Id, err)
 			return
 		}
 	case containerBrokerModels.CONTAINER_BROKER_DELETE_ROUTING_KEY:
+		instance, _ := entity.(catalogModels.Instance)
 		queueMessage, err = prepareDeleteInstanceRequest(instance)
 		if err != nil {
 			logger.Error("Failed to prepare DeleteRequest for instance: ", instance.Id, err)
 			return
 		}
 	default:
-		logger.Error(fmt.Sprintf("InstanceId: %s- following routing key is not supported: %s", instance.Id, routingKey))
+		logger.Error(fmt.Sprintf("Following routing key is not supported: %s", routingKey))
 		return
 	}
 
-	if err := queue.SendMessageToQueue(channel, queueMessage, containerBrokerModels.CONTAINER_BROKER_QUEUE_NAME, routingKey); err != nil {
-		logger.Error("Can't send message on queue! InstanceId:", instance.Id, err)
-		return
+	q.sendMessageAndReconnectIfError(queueMessage, queueName, routingKey)
+}
+
+func (q *QueueManager) sendMessageAndReconnectIfError(message []byte, queueName, routingKey string) {
+	if err := queue.SendMessageToQueue(q.Channel, message, queueName, routingKey); err != nil {
+		logger.Errorf("Can't send message on queue - err: %v! Trying to reconnect...", err)
+		channel, conn := setupQueueConnection()
+		q.Channel = channel
+		q.Connection = conn
+
+		if err := queue.SendMessageToQueue(q.Channel, message, queueName, routingKey); err != nil {
+			logger.Fatalf("Can't send message on queue! err:", err)
+
+		}
 	}
 }
 
