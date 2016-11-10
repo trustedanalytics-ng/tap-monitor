ce8f02b46b20a13f0f8650e347d28c4872aa684d
Konrad Kurdej <konrad.kurdej@intel.com>
2016-11-10 14:07:52 +0100

    Updating catalog/models to vendor

--- vendor/github.com/trustedanalytics/tap-catalog/models/image.go
+++ vendor/github.com/trustedanalytics/tap-catalog/models/image.go
@@ -1,3 +1,18 @@
+/**
+ * Copyright (c) 2016 Intel Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package models
 
 import "strings"
@@ -34,10 +49,11 @@ const (
 type ImageState string
 
 const (
-	ImageStatePending  ImageState = "PENDING"
-	ImageStateBuilding ImageState = "BUILDING"
-	ImageStateError    ImageState = "ERROR"
-	ImageStateReady    ImageState = "READY"
+	ImageStateBuilding  ImageState = "BUILDING"
+	ImageStateError     ImageState = "ERROR"
+	ImageStatePending   ImageState = "PENDING"
+	ImageStateReady     ImageState = "READY"
+	ImageStateRequested ImageState = "REQUESTED"
 )
 
 func IsApplicationInstance(imageId string) bool {
